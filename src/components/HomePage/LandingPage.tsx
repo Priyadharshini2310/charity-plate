06b15861635ff086d23594b4299ef050e1718ac6
nishanth1321 <nishanthkr134@gmail.com>
2025-11-20 16:49:12 +0530

    hero ui updated

--- src/components/HomePage/LandingPage.tsx
+++ src/components/HomePage/LandingPage.tsx
@@ -8,16 +8,16 @@
 // import Footer from "./Footer";
 // const LandingPage = () => {
 //   return (
-//     <div className="min-h-screen bg-gray-50 font-sans">
+//     <div className="min-h-screen font-sans bg-gray-50">
 //       {/* Header */}
 
 //       <header className="absolute inset-x-0 top-0 z-50">
 //         <nav // Centered the dock entirely in the nav bar
-//           className="flex items-center justify-center p-6 lg:px-8 h-20"
+//           className="flex items-center justify-center h-20 p-6 lg:px-8"
 //           aria-label="Global"
 //         >
 //           {/* Floating Dock Navigation: Centered and Aligned */}
-//           <div className="flex-1 flex justify-center h-full items-center">
+//           <div className="flex items-center justify-center flex-1 h-full">
 //             {/* <FloatingDockDemo /> */}
 //             <CharityPlateNavbar />
 //           </div>
@@ -26,10 +26,10 @@
 
 //       <main>
 //         {/* Hero Section */}
-//         <div className="relative isolate px-6 pt-14 lg:px-8">
-//           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+//         <div className="relative px-6 isolate pt-14 lg:px-8">
+//           <div className="max-w-2xl py-32 mx-auto sm:py-48 lg:py-56">
 //             <div className="text-center">
-//               <h1 className="text-6xl font-bold  text-gray-900 sm:text-7xl">
+//               <h1 className="text-6xl font-bold text-gray-900 sm:text-7xl">
 //                 Seamless Giving, Maximum Impact
 //               </h1>
 //               <p className="mt-6 text-xl leading-8 tracking-tight text-gray-600">
@@ -37,11 +37,11 @@
 //                 easy to support causes you care about and see the real-world
 //                 impact of your generosity.
 //               </p>
-//               <div className="mt-10 flex items-center justify-center gap-x-6">
-//                 <button className="px-8 py-4 bg-gradient-to-r from-sky-600 to-sky-600 text-white font-semibold rounded-xl shadow-lg hover:shadow-xl hover:from-sky-600 hover:to-sky-700 transform hover:-translate-y-1 transition-all duration-300">
+//               <div className="flex items-center justify-center mt-10 gap-x-6">
+//                 <button className="px-8 py-4 font-semibold text-white transition-all duration-300 transform shadow-lg bg-gradient-to-r from-sky-600 to-sky-600 rounded-xl hover:shadow-xl hover:from-sky-600 hover:to-sky-700 hover:-translate-y-1">
 //                   Start Donating Now
 //                 </button>
-//                 <button className="px-8 py-4 bg-white text-gray-700 font-semibold border-2 border-gray-200 rounded-xl hover:border-sky-300 hover:text-sky-700 hover:bg-sky-50/50 transition-all duration-300">
+//                 <button className="px-8 py-4 font-semibold text-gray-700 transition-all duration-300 bg-white border-2 border-gray-200 rounded-xl hover:border-sky-300 hover:text-sky-700 hover:bg-sky-50/50">
 //                   Learn More About Us
 //                 </button>
 //               </div>
@@ -59,11 +59,11 @@
 //         <FeedbackForm />
 //         {/* Call to Action */}
 //         <div className="bg-white">
-//           <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
-//             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+//           <div className="px-6 py-24 mx-auto max-w-7xl sm:py-32 lg:px-8">
+//             <h2 className="text-3xl font-bold tracking-tight text-center text-gray-900 sm:text-4xl">
 //               Ready to make a difference?
 //             </h2>
-//             <div className="mt-8 flex items-center justify-center gap-x-6">
+//             <div className="flex items-center justify-center mt-8 gap-x-6">
 //               <a
 //                 href="#"
 //                 className="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
@@ -96,27 +96,33 @@ import FeedBack from "./FeedBack";
 import ImpactStatistics from "./OurImpact";
 import TestimonialsSection from "./Testimonials";
 import Footer from "./Footer";
+import ParticlesComponent from "ui/particle";
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-50 font-sans">
+    <div className="min-h-screen font-sans bg-gray-50">
       {/* Header */}
 
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
-          className="flex items-center justify-center p-6 lg:px-8 h-20"
+          className="flex items-center justify-center h-20 p-6 lg:px-8"
           aria-label="Global"
         >
-          <div className="flex-1 flex justify-center h-full items-center">
+          <div className="flex items-center justify-center flex-1 h-full">
             <CharityPlateNavbar />
           </div>
         </nav>
       </header>
 
       <main>
+         
         {/* Hero Section */}
-        <div className="relative isolate px-6 pt-14 lg:px-8">
-          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="relative px-6 isolate pt-14 lg:px-8">
+           <div className="fixed inset-0 -z-10">
+                <ParticlesComponent id="particles-js" />
+                <div className="absolute inset-0 backdrop-blur-sm" />
+              </div>
+          <div className="max-w-2xl py-32 mx-auto sm:py-48 lg:py-56">
             <div className="text-center">
               <h1 className="text-6xl font-bold text-gray-900 sm:text-7xl">
                 Seamless Giving, Maximum Impact
@@ -126,11 +132,11 @@ const LandingPage: React.FC = () => {
                 easy to support causes you care about and see the real-world
                 impact of your generosity.
               </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6">
-                <button className="px-8 py-4 bg-gradient-to-r from-sky-600 to-sky-600 text-white font-semibold rounded-xl shadow-lg hover:shadow-xl hover:from-sky-600 hover:to-sky-700 transform hover:-translate-y-1 transition-all duration-300">
+              <div className="flex items-center justify-center mt-10 gap-x-6">
+                <button className="px-8 py-4 font-semibold text-white transition-all duration-300 transform shadow-lg bg-gradient-to-r from-sky-600 to-sky-600 rounded-xl hover:shadow-xl hover:from-sky-600 hover:to-sky-700 hover:-translate-y-1">
                   Start Donating Now
                 </button>
-                <button className="px-8 py-4 bg-white text-gray-700 font-semibold border-2 border-gray-200 rounded-xl hover:border-sky-300 hover:text-sky-700 hover:bg-sky-50/50 transition-all duration-300">
+                <button className="px-8 py-4 font-semibold text-gray-700 transition-all duration-300 bg-white border-2 border-gray-200 rounded-xl hover:border-sky-300 hover:text-sky-700 hover:bg-sky-50/50">
                   Learn More About Us
                 </button>
               </div>
@@ -149,11 +155,11 @@ const LandingPage: React.FC = () => {
 
         {/* Call to Action */}
         <div className="bg-white">
-          <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+          <div className="px-6 py-24 mx-auto max-w-7xl sm:py-32 lg:px-8">
+            <h2 className="text-3xl font-bold tracking-tight text-center text-gray-900 sm:text-4xl">
               Ready to make a difference?
             </h2>
-            <div className="mt-8 flex items-center justify-center gap-x-6">
+            <div className="flex items-center justify-center mt-8 gap-x-6">
               <a
                 href="#"
                 className="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
